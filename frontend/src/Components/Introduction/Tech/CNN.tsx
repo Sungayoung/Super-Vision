a2202cf619145a74418fa805b8bb0bf777db9fa3
Magpie1000 <web.1000c@gmail.com>
2022-03-25 20:49:27 +0900

    FEAT: 레이어 사진 컴포넌트화

--- frontend/src/Components/Introduction/Tech/CNN.tsx
+++ frontend/src/Components/Introduction/Tech/CNN.tsx
@@ -6,50 +6,61 @@ import { styled } from "@mui/material/styles";
 import Grid from '@mui/material/Grid'
 import { useTheme } from '@material-ui/core'
 
+const PageDiv = styled("div")({
+  position: "relative",
+  height: "100%",
+  width: "99.5vw",
+});
+
+type Props = {
+  isDark: boolean
+}
+
+function LayerSquare ({ isDark } : Props) {
+  return (
+    <Grid
+      
+    >
+      <div className="card" >
+        <div className="box rotateY">
+          <div className={ 'fill' + ' ' + (isDark? 'brightBox' : 'darkBox')}></div>
+        </div>
+      </div>
+    </Grid>
+  )
+}
 
 function CNN () {
   const theme = useTheme()
   console.log(theme.palette.primary.main)
   return (
-    <div>
+    <PageDiv>
       <Grid 
         className="CNN"
-        container>
+        container
+        >
         <Grid
           className="cnnName"
           ml = {10}
+          container
           item xs={2}
         >
-          <h1>Convolution</h1>
-          <h1>Neural</h1>
-          <h1>Network</h1>
+            <h1>Convolution</h1>
+            <h1>Neural</h1>
+            <h1>Network</h1>
         </Grid>
-        {/* <Grid item xs={9}>
-          <Item></Item> */}
-          <div className="card" >
-            <div className="box rotateY">
-              <div className="fill brightBox"></div>
-            </div>
-          </div>
-          <div className="card" >
-            <div className="box rotateY">
-              <div className="fill darkBox"></div>
-            </div>
-          </div>
-          <div className="card" >
-            <div className="box rotateY">
-              <div className="fill brightBox"></div>
-            </div>
-          </div>
-          <div className="card" >
-            <div className="box rotateY">
-              <div className="fill darkBox"></div>
-            </div>
-          </div>
-        {/* </Grid> */}
+        <Grid 
+          item xs={9}
+          container
+        >
+          <LayerSquare isDark={true} />
+          <LayerSquare isDark={false} />
+          <LayerSquare isDark={true} />
+          <LayerSquare isDark={false} />
+        </Grid>
       </Grid>
       
-    </div>
+    </PageDiv>
   )
 }
 
