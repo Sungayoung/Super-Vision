9462131cf054b7a00fb54ea350427f68bfa039e8
성아영 <hablobien@naver.com>
2022-03-25 17:36:19 +0900

    [Front] Fix: merge conflict 해결

--- frontend/src/Components/Commons/Btn.tsx
+++ frontend/src/Components/Commons/Btn.tsx
@@ -29,12 +29,6 @@ const CustomBtn = styled(Button)({
     backgroundColor: '#39424E',
     borderColor: '#39424E',
   },
-<<<<<<< HEAD
-  '&:focus': {
-    boxShadow: '0 0 0 0.2rem #39424E',
-  },
-=======
->>>>>>> feat/front/webcamFilter/AY
 });
 
 function Btn({ 
