31742a409cd1a8219c9f520afb1e00370ce9932a
성아영 <hablobien@naver.com>
2022-04-04 15:31:46 +0900

    [Front] Feat: markdown 커스텀

--- frontend/src/Pages/Introduction/QuickStart.tsx
+++ frontend/src/Pages/Introduction/QuickStart.tsx
@@ -1,7 +1,31 @@
+import MarkdownRenderer from "../../Components/Commons/MarkdownRenderer";
+import { useEffect, useState } from "react";
+
 function QuickStart() {
+  const [installMarkdownText, setMarkdownText] = useState<string>('')
+  const [excuteMarkdownText, setExcuteMarkdownText] = useState<string>('')
+
+  useEffect(() => {
+
+    fetch(require('../../Assets/Text/installQuickstart.md'))
+    .then(res => res.text())
+    .then(text => setMarkdownText(text))
+    
+    fetch(require('../../Assets/Text/excuteQuickstart.md'))
+    .then(res => res.text())
+    .then(text => setExcuteMarkdownText(text))
+
+  }, []);
+
   return (
-    <h1>QuickStart</h1>
-  )
+    <>
+      <h1>QuickStart</h1>
+      <div style={{width: '80%'}}>
+        <MarkdownRenderer text={installMarkdownText} />
+        <MarkdownRenderer text={excuteMarkdownText} />
+      </div>
+    </>
+  );
 }
 
-export default QuickStart
+export default QuickStart;
